e1197ac23b28de3673b99a2ffb7a13b52482ae48
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-12 23:50:55 +0530

    adding onAuthChange functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter, Switch , Route } from 'react-router-dom';
-
+import { BrowserRouter, Switch , Route, withRouter } from 'react-router-dom';
+import firebase from './firebase';
 //components
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
 
 
+class Root extends React.Component {
 
-const Root = () => (
+    //Checking Whether we have an user on the app (LoggedIn)
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user => {
+            if(user){
+                this.props.history.push('/');
+            }
+        })
+    }
+    render(){
+        return(
+                <Switch>
+                    <Route exact path="/" component={App}/>
+                    <Route  path="/login" component={Login}/>
+                    <Route  path="/register" component={Register}/>
+                </Switch>
+        );
+    }
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
     <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={App}/>
-            <Route  path="/login" component={Login}/>
-            <Route  path="/register" component={Register}/>
-        </Switch>
-    </BrowserRouter>
-)
+        <RootWithAuth />
+    </BrowserRouter>,
+document.getElementById('root'));
 
-ReactDOM.render(<Root />, document.getElementById('root'));
-registerServiceWorker();
