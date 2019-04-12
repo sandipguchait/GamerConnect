cb00026654e4d0a9b7fa37b9b95c9b7be402f912
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-12 16:38:09 +0530

    adding routes for Login and resiter components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Switch , Route } from 'react-router-dom';
+
+//components
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+
+
+
+const Root = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={App}/>
+            <Route  path="/login" component={Login}/>
+            <Route  path="/register" component={Register}/>
+        </Switch>
+    </BrowserRouter>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
