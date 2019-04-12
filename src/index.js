03bff6ed81e6232836378003c1e49c6e288d9a12
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-13 00:26:55 +0530

    importing reducer and setting up connection on index.js component with redux

--- src/index.js
+++ src/index.js
@@ -9,24 +9,32 @@ import firebase from './firebase';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
 
+//IMPORTING ALL REDUX PACKAGES
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './reducers/index';
+
+const store = createStore( rootReducer , composeWithDevTools());
 
 class Root extends React.Component {
 
-    //Checking Whether we have an user on the app (LoggedIn)
+    //Checking Whether we have an user on the app so will be redirected automatically to APP
     componentDidMount(){
         firebase.auth().onAuthStateChanged(user => {
             if(user){
+                console.log(user)
                 this.props.history.push('/');
             }
         })
     }
     render(){
         return(
-                <Switch>
-                    <Route exact path="/" component={App}/>
-                    <Route  path="/login" component={Login}/>
-                    <Route  path="/register" component={Register}/>
-                </Switch>
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route  path="/login" component={Login}/>
+                <Route  path="/register" component={Register}/>
+            </Switch>
         );
     }
 }
@@ -34,8 +42,10 @@ class Root extends React.Component {
 const RootWithAuth = withRouter(Root);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <RootWithAuth />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <RootWithAuth />
+        </BrowserRouter>
+    </Provider>,
 document.getElementById('root'));
 
