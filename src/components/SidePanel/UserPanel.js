5521d05a898300acd7f3ff5481d9ee2c38d817e5
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-15 17:41:08 +0530

    using currentUser props to display properties

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
+import firebase from '../../firebase';
 
 
 class UserPanel extends Component {
 
+    state = {
+        user: this.props.currentUser
+    }
+
     dropdownOptions = () => [
         {
             key: 'user',
-            text:<span>Signed in as <strong>User</strong></span>,
+            text:<span>Signed in as <strong>{this.state.user.displayName}</strong></span>,
             disabled: true
         },
         {
@@ -16,11 +21,19 @@ class UserPanel extends Component {
         },
         {
             key:'signout',
-            text:<span>SignOut</span>
+            text:<span onClick={this.handleSignout}>SignOut</span>
         }
     ]
 
+    handleSignout = () => {
+        firebase 
+            .auth()
+            .signOut()
+            .then(() => console.log('Signed Out'))
+    }
+
     render() {
+        const { user } = this.state;
         return (
             <Grid style={{ background: '#4c3c4c'}}>
                 <Grid.Column>
@@ -29,18 +42,26 @@ class UserPanel extends Component {
                     {/* APP HEADER */}
                         <Header inverted floated="left" as="h2">
                             <Icon name="gamepad" />
-                            <Header.Content>GConnect</Header.Content>
+                            <Header.Content>Connect</Header.Content>
                         </Header>
-                    </Grid.Row>
 
                     {/* USER DROPDOWN */}
-                    <Header style={{ padding: '0.25'}} as="h4" inverted >
-                        <Dropdown text="User" options={this.dropdownOptions()} />
-                    </Header>
+                        <Header style={{ padding: '0.25'}} as="h4" inverted >
+                            <Dropdown trigger={
+                                <span>
+                                    <Image src={user.photoURL} spaced="right" avatar />
+                                    {user.displayName}
+                                </span>
+                            }
+                                options={this.dropdownOptions()} 
+                            />
+                        </Header>
+                    </Grid.Row>
                 </Grid.Column>
             </Grid>
         );
     }
 }
+
 
 export default UserPanel;
