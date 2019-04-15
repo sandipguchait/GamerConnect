0471126bb2d54888774af1d1bd618bdc0509592d
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-15 16:13:59 +0530

    Making a MetaPanel folder with its contents

---
+++ src/components/MetaPanel/MetaPanel.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MetaPanel extends Component {
+
+    render() {
+        return (
+            <div>MetaPanel</div>
+        );
+    }
+}
+
+export default MetaPanel;
