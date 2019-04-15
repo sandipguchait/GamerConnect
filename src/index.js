96c8218ced1379b36fdc672d67a44c9c339f177a
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-15 17:42:18 +0530

    using clearUser() to push user to '/login' on signOut

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers/index';
-import { setUser } from './actions/index'; 
+import { setUser, clearUser } from './actions/index'; 
 const store = createStore( rootReducer , composeWithDevTools());
 
 class Root extends React.Component {
@@ -24,9 +24,14 @@ class Root extends React.Component {
     //IF user is Logged In then automatically user is redirected to APP component 
     componentDidMount(){
         firebase.auth().onAuthStateChanged(user => {
+            //If there is user we redirect to App component
             if(user){
                 this.props.setUser(user)
                 this.props.history.push('/');
+            } else {
+                // If we logOut OR theres is no  user will push to "/login "page
+                this.props.history.push('/login');
+                this.props.clearUser();
             }
         })
     }
@@ -49,7 +54,7 @@ const mapStateToProps = state => {
    }
 }
 
-const RootWithAuth = withRouter(connect(mapStateToProps, {setUser})(Root));
+const RootWithAuth = withRouter(connect(mapStateToProps, {setUser, clearUser})(Root));
 
 ReactDOM.render(
     <Provider store={store}>
