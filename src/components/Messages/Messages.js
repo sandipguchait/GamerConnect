9dfedfe62170f0f85a170490e382c8ea9995bbe5
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-15 22:55:44 +0530

    connecting to database to upload data and rendering Ui of different components

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import { Segment , Comment } from 'semantic-ui-react';
+
+import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
+import firebase from '../../firebase';
 
 class Messages extends Component {
 
+    state = {
+        messageRef: firebase.database().ref('messages'),
+        channel: this.props.currentChannel,
+        user: this.props.currentUser
+    }
+
+    componentDidMount(){
+        const { channel,user } = this.state;
+
+        if( channel && user ) {
+            this.addListeners(channel.id)
+        }
+    }
+
+    addListeners = (channelId) => {
+
+    }
+
     render() {
+        const { messageRef, channel, user } = this.state;
+
         return (
-            <div>Messages</div>
+            <React.Fragment>
+                <MessagesHeader />
+
+                <Segment>
+                    <Comment.Group className="messages">
+                    {/* MESSAGES HERE */}
+                    </Comment.Group>
+                </Segment>
+
+                <MessageForm  messageRef={messageRef} currentChannel={channel} currentUser={user}/>
+            </React.Fragment>
         );
     }
 }
