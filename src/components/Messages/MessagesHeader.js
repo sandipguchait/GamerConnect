e435d994a143226ed66e2d28eade98642fa796ab
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-18 00:15:45 +0530

    showing Ui conditionally

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -4,7 +4,8 @@ import { Segment, Header, Input, Icon } from 'semantic-ui-react';
 class MessagesHeader extends Component {
 
     render() {
-        const { channelName, uniqueUsers, handleSearchChange, searchLoading , isPrivateChannel } = this.props;
+        const { channelName, uniqueUsers, handleSearchChange,
+             searchLoading , isPrivateChannel, handleStar, isChannelStarred } = this.props;
 
         return (
             <Segment clearing>
@@ -12,7 +13,13 @@ class MessagesHeader extends Component {
                 <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
                     <span>
                         {channelName} {" "}
-                        { !isPrivateChannel && <Icon name={"star outline"} color="black" />}
+                        { !isPrivateChannel && (
+                            <Icon 
+                                onClick={handleStar} 
+                                name={isChannelStarred ? 'star' : 'star outline'} 
+                                color={isChannelStarred ? 'yellow' : 'black'} 
+                            />
+                            )}
                     </span>
                     <Header.Subheader>{uniqueUsers}</Header.Subheader>
                 </Header>
