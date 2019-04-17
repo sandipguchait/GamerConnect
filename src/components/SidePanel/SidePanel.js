b0b84613eb8bd75504a782d8b2c4152edf9bd265
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-17 21:40:32 +0530

    making Starred component and importing

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -3,6 +3,7 @@ import { Menu } from 'semantic-ui-react';
 import UserPanel from './UserPanel';
 import Channels from './Channels';
 import DirectMessages from './DirectMessages';
+import Starred from './Starred';
 
 class Sidepanel extends Component {
 
@@ -19,6 +20,7 @@ class Sidepanel extends Component {
                 <UserPanel currentUser={currentUser} />
                 <Channels currentUser={currentUser} />
                 <DirectMessages currentUser={currentUser}/>
+                <Starred />
             </Menu>
         );
     }
